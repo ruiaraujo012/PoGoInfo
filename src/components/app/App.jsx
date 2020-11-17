b19ee29e1559c5e44e5849ad9a346b75856bba24
Rui Araújo <ruiaraujo012@gmail.com>
2020-11-17 19:06:44 +0000

    :sparkless: Navbar added

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,17 @@
+import React from "react";
 import "./App.scss";
 
-function App() {
+import Navbar from "components/navbar/Navbar";
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <h2>PoGoInfo</h2>
       </header>
     </div>
   );
-}
+};
 
 export default App;
