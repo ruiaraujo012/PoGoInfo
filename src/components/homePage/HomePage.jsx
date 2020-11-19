119507e39a609e2c7ec8909aa421640236f86d43
Rui Araújo <ruiaraujo012@gmail.com>
2020-11-19 17:51:04 +0000

    ♻️ Refactor some code

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -9,7 +9,6 @@ import operations from "resources/operations";
 
 const useStyles = makeStyles({
   root: {
-    flexGrow: 1,
     marginTop: "5%",
     marginBottom: "5%",
   },
@@ -19,19 +18,17 @@ const HomePage = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container justify="center">
-        <Grid item md={2} />
-        <Grid item container alignContent="center" spacing={3} xs={12} sm={8}>
-          {operations.map((op, i) => (
-            <Grid item xs={12} sm={6} md={4} key={i}>
-              <CardLink operation={op} />
-            </Grid>
-          ))}
-        </Grid>
-        <Grid item md={2} />
+    <Grid container justify="center" className={classes.root}>
+      <Grid item md={2} />
+      <Grid item container alignContent="center" spacing={3} xs={12} sm={8}>
+        {operations.map((op, i) => (
+          <Grid item xs={12} sm={6} md={4} key={i}>
+            <CardLink operation={op} />
+          </Grid>
+        ))}
       </Grid>
-    </div>
+      <Grid item md={2} />
+    </Grid>
   );
 };
 
