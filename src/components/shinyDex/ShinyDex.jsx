7233403f54a0e78f29f341f6474c40206b130310
Rui Araújo <ruiaraujo012@gmail.com>
2020-11-25 21:55:58 +0000

    :sparkles: Pagination added on shiny dex

--- src/components/shinyDex/ShinyDex.jsx
+++ src/components/shinyDex/ShinyDex.jsx
@@ -3,6 +3,7 @@ import pogoapi from "api/pogoapi";
 import { Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PokemonCard from "components/shinyDex/PokemonCard";
+import { Pagination } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,28 +18,55 @@ const useStyles = makeStyles((theme) => ({
   marginAround: {
     margin: "0px",
   },
+  pagination: {
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 const ShinyDex = () => {
   const classes = useStyles();
 
-  const [shinyDex, setShinyDex] = useState({});
+  // Change to state if needed (dynamic number)
+  const POKEMONS_PER_PAGE = 20;
+
+  const [shinyDex, setShinyDex] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    fetchShinyDex();
-    setLoading(false);
-  }, []);
-
+  // Functions and methods
   const fetchShinyDex = async () => {
     try {
       const { data } = await pogoapi.get("/shiny_pokemon.json");
-      setShinyDex(data);
+      let test = Object.values(data);
+      setShinyDex(test);
     } catch (err) {
       // TODO: Add toast notification
       console.log("err :>> ", err);
     }
   };
+
+  const handleChangePage = (event, value) => {
+    setCurrentPage(value);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  // Effect
+  useEffect(() => {
+    fetchShinyDex();
+    setLoading(false);
+  }, []);
+
+  // Get current pokemons
+  const indexOfLastPokemon = currentPage * POKEMONS_PER_PAGE;
+  const indexOfFirstPokemon = indexOfLastPokemon - POKEMONS_PER_PAGE;
+  const currentShinyDex = shinyDex.slice(
+    indexOfFirstPokemon,
+    indexOfLastPokemon
+  );
+  const totalPages = Math.ceil(shinyDex.length / POKEMONS_PER_PAGE);
 
   return (
     <Grid container justify="center" className={classes.root}>
@@ -54,13 +82,33 @@ const ShinyDex = () => {
             spacing={3}
           >
             {!loading &&
-              Object.keys(shinyDex).map((key, i) => (
+              currentShinyDex.map((pokemon, i) => (
                 <Grid item xs={12} sm={4} md={3} lg={4} key={i}>
-                  <PokemonCard pokemon={shinyDex[key]} key={i} />
+                  <PokemonCard pokemon={pokemon} key={i} />
                 </Grid>
               ))}
           </Grid>
         </Paper>
+
+        <Grid
+          item
+          container
+          xs={12}
+          lg={8}
+          justify="center"
+          className={classes.pagination}
+        >
+          <Grid item>
+            <Pagination
+              count={totalPages}
+              color="primary"
+              showFirstButton
+              showLastButton
+              siblingCount={0}
+              onChange={handleChangePage}
+            />
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item lg={2} />
     </Grid>
